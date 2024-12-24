b0b0a3b26d8057b104db876c8c139730ad553da6
leehuanyu <4b2g0001@stust.edu.tw>
2024-12-24 15:12:49 +0800

    更新 MyDocumentViewer 和 MainWindow 的 UI 和事件處理
    - 將 MainWindow.xaml 中的標題從 "2023小作家ver1" 改為 "2023小作家ver2"。
    - 在 MyDocumentViewer.xaml 中，為粗體、斜體和底線按鈕新增了對應的命令。
    - 新增字體顏色選擇器、字體選擇框和字體大小選擇框的事件處理程序。
    - 新增 RichTextBox 的 `SelectionChanged` 事件處理程序。
    - 在 MyDocumentViewer.xaml.cs 中，新增了相應的事件處理方法。
    - 在 `rtbEditor_SelectionChanged` 方法中，更新按鈕狀態和選擇框。

--- 2024_WpfApp5/MyDocumentViewer.xaml.cs
+++ 2024_WpfApp5/MyDocumentViewer.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace _2024_WpfApp5
@@ -38,6 +39,50 @@ namespace _2024_WpfApp5
         private void SaveCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
 
+        }
+
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontColor = (Color)e.NewValue;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontColor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            }
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox.SelectedItem != null)
+            {
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            }
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            var property_bold = rtbEditor.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldButton.IsChecked = (property_bold != DependencyProperty.UnsetValue) && (property_bold.Equals(FontWeights.Bold));
+
+            var property_italic = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicButton.IsChecked = (property_italic != DependencyProperty.UnsetValue) && (property_italic.Equals(FontStyles.Italic));
+
+            var property_underline = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (property_underline != DependencyProperty.UnsetValue) && (property_underline.Equals(TextDecorations.Underline));
+
+            var property_fontFamily = rtbEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            fontFamilyComboBox.SelectedItem = property_fontFamily.ToString();
+
+            var property_fontSize = rtbEditor.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            fontSizeComboBox.SelectedItem = property_fontSize;
+
+            var property_fontColor = rtbEditor.Selection.GetPropertyValue(TextElement.ForegroundProperty);
+            fontColorPicker.SelectedColor = ((SolidColorBrush)property_fontColor).Color;
         }
     }
 }
